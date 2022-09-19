3cc2898b452909bddd4d883480fb501798a0e4cb
Alan <mejia.alan.r@gmail.com>
2022-09-19 10:44:04 -0300

    Arreglar mensaje de error al registarse

--- src/users/actions/register.js
+++ src/users/actions/register.js
@@ -1,66 +1,92 @@
-import { v4 as uuidv4 } from 'uuid';
-import { fetchSinToken } from '../../shared/helpers/fetch';
-import { types } from './../../shared/types/types';
-
+import { v4 as uuidv4 } from "uuid";
+import { fetchSinToken } from "../../shared/helpers/fetch";
+import { types } from "./../../shared/types/types";
 
 // Primary Actions
 
-const sendRegister = ( ) => ({
-  type: types.sendRegister,
+const sendRegister = () => ({
+    type: types.sendRegister,
 });
 
-const sendRegisterSuccess = ( msg ) => ({
-  type: types.sendRegister_success,
-  payload: msg,
+const sendRegisterSuccess = (msg) => ({
+    type: types.sendRegister_success,
+    payload: msg,
 });
 
-const sendRegisterError = ( msg ) => ({
-  type: types.sendRegister_error,
-  payload: msg,
+const sendRegisterError = (msg) => ({
+    type: types.sendRegister_error,
+    payload: msg,
 });
 
-export const sendRegisterClean = ( ) => ({
-  type: types.sendRegister_clean,
+export const sendRegisterClean = () => ({
+    type: types.sendRegister_clean,
 });
-
 
 // Async Actions
 
-export const startSendRegister = ( dataForm ) => {
-  return async( dispatch ) => {
-    dispatch( sendRegister( ) );
-    try{
-      // TODO : Refactorizar
-      const data = {
-        ...dataForm,
-        uuid : uuidv4( ),
-      };
+export const startSendRegister = (dataForm) => {
+    return async (dispatch) => {
+        dispatch(sendRegister());
+        try {
+            // TODO : Refactorizar
+            const data = {
+                ...dataForm,
+                uuid: uuidv4(),
+            };
 
-      // TODO : Refactorizar envio de informacion al servidor
-      const resp = await fetchSinToken( 
-        'users', 
-        { 
-          email: data.email, 
-          password: data.password, 
-          firstname: data.name, 
-          lastname: data.surname, 
-          id: data.uuid,
-        },
-        'POST',
-      );
-      const body = await resp.json( );
-      
-      if( body?.status === 'success' ){
-        let msg = 'El registro ha sido exitoso. En breve recibirá un correo electrónico para verificar su cuenta.';
-        dispatch( sendRegisterSuccess( msg ) );
-      }else{
-        let errorMsg = 'Ha ocurrido un error en el registro de la cuenta.';
-        dispatch( sendRegisterError( errorMsg ) );
-      }
+            // TODO : Refactorizar envio de informacion al servidor
+            const resp = await fetchSinToken(
+                "users",
+                {
+                    email: data.email,
+                    password: data.password,
+                    firstname: data.name,
+                    lastname: data.surname,
+                    id: data.uuid,
+                },
+                "POST"
+            );
+            const body = await resp.json();
 
-    }catch( err ){
-      let errorMsg = 'Ha ocurrido un error en el registro de la cuenta.';
-      dispatch( sendRegisterError( errorMsg ) );
+            if (body?.status === "success") {
+                let msg =
+                    "El registro ha sido exitoso. En breve recibirá un correo electrónico para verificar su cuenta.";
+                dispatch(sendRegisterSuccess(msg));
+            } else {
+                let errorDefaultMsg =
+                    "Ha ocurrido un error en el registro de la cuenta.";
+
+                // console.log(body);
+
+                let response = await fetchSinToken("error-codes");
+                let errors = await response.json();
+                let errorMessage = errors.data.errors.find(
+                    (errorCode) => errorCode.code === body.data.code
+                )?.description;
+
+                if (errorMessage) {
+                    errorMessage = (
+                        <p>
+                            {errorMessage.replace(
+                                '"¿Olvidó su contraseña?"',
+                                ""
+                            )}
+                            :{" "}
+                            <a href="https://www.ign.gob.ar/ppp/auth/recoverPassword">
+                                ¿Olvidó su contraseña?
+                            </a>
+                        </p>
+                    );
+                }
+                dispatch(
+                    sendRegisterError(
+                        errorMessage ? errorMessage : errorDefaultMsg
+                    )
+                );
+            }
+        } catch (err) {
+            let errorMsg = "Ha ocurrido un error en el registro de la cuenta.";
+            dispatch(sendRegisterError(errorMsg));
+        }
     };
-  };
 };
